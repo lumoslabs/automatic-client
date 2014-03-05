cbbd58de11ccd25c1b1b145dd2aaaeeabc131e0c
Nate Klaiber <nate@theklaibers.com>
2014-03-05 17:39:54 -0500

    WIP: First pass at creating a CSV export from the trips

--- lib/automatic/command_line/trips.rb
+++ lib/automatic/command_line/trips.rb
@@ -1,3 +1,4 @@
+require 'csv'
 require 'thor'
 require 'terminal-table'
 
@@ -6,6 +7,36 @@ require File.expand_path('../../client', __FILE__)
 module Automatic
   module CommandLine
     class Trips < Thor
+      desc 'export', 'Export Automatic Trips to CSV'
+      option :filename, type: :string, banner: 'trips-{timestamp}.csv', default: nil
+      def export
+        trips = Automatic::Client::Trips.all
+
+        if trips.any?
+          default_filename = "trips-%s.csv" % [Time.now.utc.to_i]
+
+          filename = (options[:filename] || default_filename)
+
+          trip_row = ->(record) do
+            [record.id, record.vehicle.display_name, record.start_location.lat, record.start_location.lon, record.end_location.lat, record.end_location.lon, record.start_at, record.end_at, record.elapsed_time, record.average_mpg, record.fuel_cost, record.distance_in_miles]
+          end
+
+          headings = ['ID', 'Vehicle', 'Start Latitude', 'Start Longitude', 'End Latitude', 'End Longitude', 'Start Time', 'End Time', 'Duration', 'Average MPG', 'Fuel Cost', 'Distance']
+
+          CSV.open(filename, "w+") do |csv|
+            csv << headings
+
+            trips.each do |trip|
+              csv << trip_row.call(trip)
+            end
+          end
+
+          puts "Success! Trips have been exported to %s" % [filename]
+        else
+          puts "No Trips to export."
+        end
+      end
+
       desc 'all', 'List all Automatic Trips'
       def all
         puts "\n"
