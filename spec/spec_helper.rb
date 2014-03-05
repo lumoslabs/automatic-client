d4a327e9d84e15d18f300276d25461c953a01304
Nate Klaiber <nate@theklaibers.com>
2014-03-05 11:07:50 -0500

    Adding the spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'webmock/rspec'
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+require 'automatic/client'
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    WebMock.disable!
+  end
+end
