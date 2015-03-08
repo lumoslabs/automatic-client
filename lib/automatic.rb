a7d66a279eab89c119b25b66ed3116607182ce59
Nate Klaiber <nate@theklaibers.com>
2015-03-08 18:44:14 -0400

    Remove root require and move to Client module

--- lib/automatic.rb
+++ lib/automatic.rb
@@ -1,7 +1,4 @@
 require File.expand_path('../automatic/client', __FILE__)
 
-require File.expand_path('../automatic/scopes', __FILE__)
-require File.expand_path('../automatic/scope', __FILE__)
-
 module Automatic
 end
