fd7a52c4327b4708b02b8a804248698f750daeef
Nate Klaiber <nate@theklaibers.com>
2015-03-08 10:59:06 -0400

    Update routes

--- lib/automatic/client.rb
+++ lib/automatic/client.rb
@@ -36,10 +36,10 @@ module Automatic
 
     def self.routes
       routes = RestlessRouter::Routes.new
-      routes.add_route(RestlessRouter::Route.new('trips', 'https://api.automatic.com/v1/trips/{?page,per_page}', templated: true))
-      routes.add_route(RestlessRouter::Route.new('trip', 'https://api.automatic.com/v1/trips/{id}', templated: true))
-      routes.add_route(RestlessRouter::Route.new('vehicles', 'https://api.automatic.com/v1/vehicles/{?page,per_page}', templated: true))
-      routes.add_route(RestlessRouter::Route.new('vehicle', 'https://api.automatic.com/v1/vehicles/{id}', templated: true))
+      routes.add_route(RestlessRouter::Route.new('trips', 'https://api.automatic.com/trip/{?page,per_page}', templated: true))
+      routes.add_route(RestlessRouter::Route.new('trip', 'https://api.automatic.com/trip/{id}', templated: true))
+      routes.add_route(RestlessRouter::Route.new('vehicles', 'https://api.automatic.com/vehicle/{?page,per_page}', templated: true))
+      routes.add_route(RestlessRouter::Route.new('vehicle', 'https://api.automatic.com/vehicle/{id}', templated: true))
       routes
     end
   end
