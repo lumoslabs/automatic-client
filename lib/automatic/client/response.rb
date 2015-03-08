721d81cfa7e98a4a3d9e686a4e0f9e62e0521091
Nate Klaiber <nate@theklaibers.com>
2015-03-08 11:11:12 -0400

    Adds the `_metadata` object from the response body

--- lib/automatic/client/response.rb
+++ lib/automatic/client/response.rb
@@ -1,6 +1,7 @@
 require 'automatic/client/response/link_header'
 require 'automatic/client/response/links'
 require 'automatic/client/response/link'
+require 'automatic/client/response/metadata'
 
 module Automatic
   module Client
