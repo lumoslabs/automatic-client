5510cbac3a77d084bef480f3b52732fb710536f2
Nate Klaiber <nate@theklaibers.com>
2015-03-08 16:04:10 -0400

    Adding `find_by_id` for user and the CLI counterpart

--- lib/automatic/client/users.rb
+++ lib/automatic/client/users.rb
@@ -3,6 +3,24 @@ module Automatic
     class Users
       include Enumerable
 
+      def self.find_by_id(id, options={})
+        route = Automatic::Client.routes.route_for('user')
+
+        request       = Automatic::Client::Request.get(route.url_for(id: id), options)
+        response      = request
+        response_body = MultiJson.load(response.body)
+
+        case(response.status)
+        when 200
+          Automatic::Client::User.new(response_body)
+        else
+          response_body.merge!('status' => response.status)
+          error = Automatic::Client::Error.new(response_body)
+
+          raise StandardError.new(error.full_message)
+        end
+      end
+
       def initialize(collection)
         @collection = Array(collection)
       end
