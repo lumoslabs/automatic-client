b646ba26c1cd55eaa0b77728a21c3cf4adf63535
Nate Klaiber <nate@theklaibers.com>
2015-04-03 15:39:11 -0400

    Extract the Request object out of Vehicles

--- lib/automatic/models/vehicles.rb
+++ lib/automatic/models/vehicles.rb
@@ -3,76 +3,96 @@ module Automatic
     class Vehicles
       include Enumerable
 
+      RecordNotFoundError = Class.new(StandardError)
+
+      # Creates a new instance of the Vehicles collection. This is
+      # used to wrap the vehicles and allow extra support for finders,
+      # sorting, and grouping
+      #
+      # @param collection [Array] A collection of Automatic Vehicle Definitions
+      #
+      # @return [Automatic::Models::Vehicles] Instance of the object
       def initialize(collection)
         @collection = Array(collection)
       end
 
+      # Find a Vehicle by the specified ID
+      #
+      # @param id [String] The Automatic Vehicle ID
+      # @param options [Hash] HTTP Query String Parameters
+      #
+      # @return [Automatic::Models::Vehicle,Nil]
       def self.find_by_id(id, options={})
-        id = id.to_s
+        vehicle_route = Automatic::Client.routes.route_for('vehicle')
+        vehicle_url   = vehicle_route.url_for(id: id)
 
-        route = Automatic::Client.routes.route_for('vehicle')
+        request = Automatic::Client.get(vehicle_url, options)
 
-        request   = Automatic::Client::Request.get(route.url_for(id: id), options)
-        response  = request
-        json_body = MultiJson.load(response.body)
-
-        case(response.status)
-        when 200
-          Automatic::Models::Vehicle.new(json_body)
+        if request.success?
+          Automatic::Models::Vehicle.new(request.body)
         else
-          json_body.merge!('status' => response.status)
-          error = Automatic::Models::Error.new(json_body)
-
-          raise StandardError.new(error.full_message)
+          nil
         end
       end
 
+      # Find a Vehicle by the specified ID
+      #
+      # @param id [String] The Automatic Vehicle ID
+      # @param options [Hash] HTTP Query String Parameters
+      #
+      # @raise [RecordNotFoundError] if no Vehicle can be found
+      #
+      # @return [Automatic::Models::Vehicle,Nil]
+      def self.find_by_id!(id, options={})
+        vehicle = self.find_by_id(id, options)
+
+        raise RecordNotFoundError.new("Could not find Vehicle with ID %s" % [id]) if vehicle.nil?
+
+        vehicle
+      end
+
+      # Make a connection to Automatic to retrieve all vehicles.
+      #
+      # @param options [Hash] Options to send to the HTTP Request
+      #
+      # @return [Automatic::Models::Vehicles] Automatic Vehicles Model
       def self.all(options={})
-        route = Automatic::Client.routes.route_for('vehicles')
+        vehicles_route = Automatic::Client.routes.route_for('vehicles')
+        vehicles_url   = vehicles_route.url_for
 
-        request   = Automatic::Client::Request.get(route.url_for, options)
-        response  = request
-        json_body = MultiJson.load(response.body)
+        request = Automatic::Client.get(vehicles_url, options)
 
-        case(response.status)
-        when 200
+        if request.success?
           raw_vehicles = []
 
-          metadata    = Automatic::Models::Response::Metadata.new(json_body.fetch('_metadata', {}))
-          link_header = Automatic::Models::Response::LinkHeader.new(response.headers['Link'])
+          metadata    = Automatic::Models::Response::Metadata.new(request.body.fetch('_metadata', {}))
+          link_header = Automatic::Models::Response::LinkHeader.new(request.headers['Link'])
           links       = link_header.links
 
-          raw_vehicles.concat(json_body.fetch('results', []))
+          raw_vehicles.concat(request.body.fetch('results', []))
 
           if links.next?
             loop do
-              request   = Automatic::Client::Request.get(links.next.uri, options)
-              response  = request
-              json_body = MultiJson.load(response.body)
+              request = Automatic::Client.get(links.next.uri)
 
-              link_header = Automatic::Models::Response::LinkHeader.new(response.headers['Link'])
+              link_header = Automatic::Models::Response::LinkHeader.new(request.headers['Link'])
               links       = link_header.links
 
-              raw_vehicles.concat(json_body.fetch('results', []))
+              raw_vehicles.concat(request.body.fetch('results', []))
 
               break unless links.next?
             end
           end
 
           self.new(raw_vehicles)
-        when 403
-          json_body.merge!('status' => 403)
-          error = Automatic::Models::Error.new(json_body)
-
-          raise UnauthorizedError.new(error.full_message)
         else
-          json_body.merge!('status' => 403)
-          error = Automatic::Models::Error.new(json_body)
-
-          raise StandardError.new(error.full_message)
+          raise StandardError.new(request.body)
         end
       end
 
+      # Implemented to support Enumerable
+      #
+      # @return [Array,Automatic::Models::Vehicle]
       def each(&block)
         vehicles_collection.each(&block)
       end
