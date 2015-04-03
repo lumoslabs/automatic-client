949ca14cfda47657ece41fdf9839efc65ff2ed9a
Nate Klaiber <nate@theklaibers.com>
2015-04-03 13:04:02 -0400

    Adding the root Models Namespace

---
+++ lib/automatic/models.rb
@@ -0,0 +1,29 @@
+require "automatic/models/error"
+
+require "automatic/models/polyline"
+
+require "automatic/models/response"
+
+require "automatic/models/trips"
+require "automatic/models/trip"
+
+require "automatic/models/vehicles"
+require "automatic/models/vehicle"
+
+require "automatic/models/vehicle_events"
+require "automatic/models/vehicle_event"
+
+require "automatic/models/users"
+require "automatic/models/user"
+
+require "automatic/models/user_metadata"
+
+require "automatic/models/locations"
+require "automatic/models/location"
+
+require "automatic/models/address"
+
+module Automatic
+  module Models
+  end
+end
