0225b176ab62f3e6612097b958ea11e689aec210
Nate Klaiber <nate@theklaibers.com>
2015-04-03 15:39:58 -0400

    Wrapping many of the helper methods to the core Client
    There will be some more extractions, but or now
    this gives a single point to begin refactoring.

--- lib/automatic/client.rb
+++ lib/automatic/client.rb
@@ -7,51 +7,128 @@ require 'restless_router'
 require 'dotenv'
 Dotenv.load
 
-require 'automatic/core'
-
-require 'automatic/configuration'
-
 require "automatic/client/version"
+require "automatic/middleware"
+require "automatic/core_extension"
+require "automatic/utilities"
+require 'automatic/uri_proxy'
+require 'automatic/core'
+require 'automatic/configuration'
+require 'automatic/connection'
+require 'automatic/response'
+require 'automatic/models'
 
 require "automatic/client/request"
-require "automatic/client/error"
-
-require "automatic/client/response"
-
-require "automatic/client/polyline"
-
-require "automatic/client/trips"
-require "automatic/client/trip"
-
-require "automatic/client/vehicles"
-require "automatic/client/vehicle"
-
-require "automatic/client/users"
-require "automatic/client/user"
-
-require "automatic/client/user_metadata"
-
-require "automatic/client/locations"
-require "automatic/client/location"
-
-require "automatic/client/address"
-
-require "automatic/core_extension"
-
-require "automatic/utilities"
 
 module Automatic
   module Client
     class << self
       attr_accessor :configuration
+      attr_accessor :scopes
+      attr_accessor :webhooks
+      attr_accessor :errors
+      attr_accessor :routes
     end
 
+    # Configuration details for them and interacting
+    # with the Automatic API.
+    #
+    # @return [Automatic::Configuration] Configuration with defaults applied
     def self.configuration
       @configuration ||= Automatic::Configuration.new
     end
 
+    # Modify the configuration details via a `block`
+    #
+    # @return [Automatic::Configuration]
     def self.configure
       yield(self.configuration)
+    end
+
+    # Helper method to build the core Connection to the API
+    # itself.
+    #
+    # @return [Automatic::Connection] A Connection delegator to Faraday
+    def self.connection
+      @connection ||= Automatic::Connection.new(url: self.configuration.api_host) do |builder|
+        builder.request(:json)
+        builder.response(:json, content_type: /\bjson$/)
+        builder.response(:logger, self.configuration.request_logger) if self.configuration.request_logger
+        builder.use(FaradayMiddleware::FollowRedirects, limit: 3)
+        builder.use(Automatic::Middleware::Gzip)
+        builder.adapter(Automatic::Connection.default_adapter)
+      end
+
+      # Inject Authorization
+      @connection.headers['Authorization'] = ("Bearer %s" % [self.configuration.access_token])
+
+      # Inject default headers
+      @connection.headers.merge!(self.configuration.connection_options[:headers])
+
+      @connection
+    end
+
+    # Helper method to peform a GET request
+    #
+    # @return [Automatic::Response] Faraday Response Delegator
+    def self.get(url, data={}, headers={})
+      request = self.connection.get(url, data, headers)
+
+      Automatic::Response.new(request)
+    end
+
+    # Helper method to perform a HEAD request
+    #
+    # @return [Automatic::Response] Faraday Response Delegator
+    def self.head(url, data={}, headers={})
+      request = self.connection.head(url, data, headers)
+
+      Automatic::Response.new(request)
+    end
+
+    # Helper method to perform a OPTIONS request
+    #
+    # @return [Automatic::Response] Faraday Response Delegator
+    def self.options(url, headers={})
+      request = self.connection.http_options(url, nil, headers)
+
+      Automatic::Response.new(request)
+    end
+
+    # Helper method to perform a PUT request
+    #
+    # @return [Automatic::Response] Faraday Response Delegator
+    def self.put(url, data={}, headers={})
+      request = self.connection.put(url, data, headers)
+
+      Automatic::Response.new(request)
+    end
+
+    # Helper method to perform a PATCH request
+    #
+    # @return [Automatic::Response] Faraday Response Delegator
+    def self.patch(url, data={}, headers={})
+      request = self.connection.patch(url, data, headers)
+
+      Automatic::Response.new(request)
+    end
+
+    # Helper method to perform a POST request
+    #
+    # @return [Automatic::Response] Faraday Response Delegator
+    def self.post(url, data={}, headers={})
+      request = self.connection.post(url, data, headers)
+
+      Automatic::Response.new(request)
+    end
+
+    # Helper method to perform a DELETE request
+    #
+    # @return [Automatic::Response] Faraday Response Delegator
+    def self.delete(url, data={}, headers={})
+      request = self.connection.delete(url, data, headers)
+
+      Automatic::Response.new(request)
     end
 
     def self.scopes
