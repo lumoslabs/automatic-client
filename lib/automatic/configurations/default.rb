6088b05f0a87fa5fc6506d141b6812d7f99ba3ff
Nate Klaiber <nate@theklaibers.com>
2015-04-03 13:05:03 -0400

    Adding yardoc to the default configuration

--- lib/automatic/configurations/default.rb
+++ lib/automatic/configurations/default.rb
@@ -2,6 +2,8 @@ require 'logger'
 
 module Automatic
   module Configurations
+    # This module is designated to setting the default values
+    # for the Client Configuration
     module Default
       API_HOST = "https://api.automatic.com".freeze
 
@@ -15,30 +17,52 @@ module Automatic
       MEDIA_TYPE   = "application/json"
       CONTENT_TYPE = "application/json"
 
+      # Return a hash of default keys and values
+      #
+      # @return [Hash] Configuration keys and default values
       def self.options
         Hash[Automatic::Configuration.keys.map { |key| [key, __send__(key)] }]
       end
 
+      # Returns the default access token
+      #
+      # @return [String,nil]
       def self.access_token
         ENV['AUTOMATIC_ACCESS_TOKEN']
       end
 
+      # Returns the specified API host or the default
+      # production domain.
+      #
+      # @return [String]
       def self.api_host
         ENV['AUTOMATIC_API_HOST'] || API_HOST
       end
 
+      # Returns the specified media type or the default.
+      #
+      # @return [String]
       def self.media_type
         ENV['AUTOMATIC_MEDIA_TYPE'] || MEDIA_TYPE
       end
 
+      # Returns the specified content type or the default.
+      #
+      # @return [String]
       def self.content_type
         ENV['AUTOMATIC_CONTENT_TYPE'] || CONTENT_TYPE
       end
 
+      # Returns the specified User Agent or the default
+      #
+      # @return [String]
       def self.user_agent
         ENV['AUTOMATIC_USER_AGENT'] || USER_AGENT
       end
 
+      # Returns the specified auto pagination or false
+      #
+      # @return [Boolean]
       def self.auto_paginate
         auto_paginate_value = ENV['AUTOMATIC_AUTO_PAGINATE']
 
@@ -55,22 +79,39 @@ module Automatic
         auto_paginate
       end
 
+      # Returns the default HTTP cache logger
+      #
+      # @note This is only applicable if caching is enabled
+      #
+      # @return [Logger] Log to STDOUT
       def self.cache_logger
         Logger.new(STDOUT)
       end
 
+      # Returns the default HTTP request logger
+      #
+      # @return [Logger] Log to STDOUT
       def self.request_logger
         Logger.new(STDOUT)
       end
 
+      # Return the default Gem Application Logger
+      #
+      # @return [Logger] Log to STDOUT
       def self.application_logger
         Logger.new(STDOUT)
       end
 
+      # Return the default middleware stack
+      #
+      # @return [Faraday::RackBuilder] Faraday Rack Builder
       def self.middleware
         MIDDLEWARE
       end
 
+      # Return the default set of connection options
+      #
+      # @return [Hash]
       def self.connection_options
         {
           :headers => {
