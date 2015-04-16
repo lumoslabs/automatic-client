75d49046a6da22f7f7c2e545d177c867d1efbd21
Nate Klaiber <nate@theklaibers.com>
2015-04-16 11:47:26 -0400

    Adding documentation to VehicleEvents model

--- lib/automatic/models/vehicle_events.rb
+++ lib/automatic/models/vehicle_events.rb
@@ -5,16 +5,28 @@ module Automatic
     class VehicleEvents
       include Enumerable
 
+      # Return a polymorphic collection of Automatic Event domain models
+      #
+      # @param collection [Array] Array of JSON Automatic Event models
+      #
+      # @return [Automatic::Models::VehicleEvents]
       def initialize(collection)
         @collection = Array(collection)
       end
 
+      # Implement #each for Enumerable support
+      #
+      # @return [Array]
       def each(&block)
         records_collection.each(&block)
       end
 
+      # Return all events of the specified type
+      #
+      # @note Return `self` so we can chain information.
+      #
+      # @return [Array]
       def find_all_by_type(type)
-        # NOTE: Return an instance of self
         self.select { |record| record.type == type.to_s }
       end
 
