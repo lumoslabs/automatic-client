458ec13ff1746a4c87a8d90d2890f2ab16932ee9
Nate Klaiber <nate@theklaibers.com>
2015-04-16 11:03:46 -0400

    Require the HardAccel and HardBrake events

--- lib/automatic/models/vehicle_event.rb
+++ lib/automatic/models/vehicle_event.rb
@@ -1,4 +1,6 @@
 require File.expand_path('../vehicle_event/notification_speeding', __FILE__)
+require File.expand_path('../vehicle_event/notification_hard_accel', __FILE__)
+require File.expand_path('../vehicle_event/notification_hard_brake', __FILE__)
 
 module Automatic
   module Models
