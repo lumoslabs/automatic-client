c43f37772ff8f521b96db06403782c5211764a0d
Nate Klaiber <nate@theklaibers.com>
2015-04-16 11:39:46 -0400

    Adding documentation to the core Error model

--- lib/automatic/models/error.rb
+++ lib/automatic/models/error.rb
@@ -1,18 +1,32 @@
 module Automatic
   module Models
     class Error
+      # Returns an instance of an Error domain model
+      #
+      # @param attributes [Hash] HTTP Error model
+      #
+      # @return [Automatic::Models::Error]
       def initialize(attributes={})
         @attributes = attributes
       end
 
+      # Returns the HTTP status of the error
+      #
+      # @return [String]
       def status
         @attributes.fetch('status', nil)
       end
 
+      # Returns the message of the error
+      #
+      # @return [String]
       def message
         @attributes.fetch('message', nil)
       end
 
+      # Returns the full message of the error
+      #
+      # @return [String]
       def full_message
         "Status Code: %s, Message: %s" % [self.status, self.message]
       end
