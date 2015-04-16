656db7074f8c7ad707a02241d6c9378d63900286
Nate Klaiber <nate@theklaibers.com>
2015-04-16 11:41:30 -0400

    Adding documentation to the core Locations model

--- lib/automatic/models/locations.rb
+++ lib/automatic/models/locations.rb
@@ -3,10 +3,19 @@ module Automatic
     class Locations
       include Enumerable
 
+      # Returns a collection Proxy for Location domain models
+      #
+      # @param collection [Array] Array of JSON Automatic Location
+      # Definitions
+      #
+      # @return [Automatic::Models::Locations]
       def initialize(collection)
         @collection = Array(collection)
       end
 
+      # Implement #each for Enumerable support
+      #
+      # @return [Array]
       def each(&block)
         locations_collection.each(&block)
       end
