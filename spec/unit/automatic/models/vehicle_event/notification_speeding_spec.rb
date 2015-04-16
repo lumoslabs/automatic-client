9ed93e56e8ada73887154a4d297aeb8ed4250f98
Nate Klaiber <nate@theklaibers.com>
2015-04-16 10:58:34 -0400

    Adding the NotificationSpeeding sample data file

---
+++ spec/unit/automatic/models/vehicle_event/notification_speeding_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe Automatic::Models::VehicleEvent::NotificationSpeeding do
+  let(:event_file) do
+    File.read(File.expand_path('../../../../../data/vehicle_event/notification_speeding.json', __FILE__))
+  end
+
+  let(:event) do
+    MultiJson.load(event_file)
+  end
+
+  let(:attributes) { event }
+
+  subject { described_class.new(attributes) }
+
+  context "with all values" do
+    it "returns the #start_distance in meters" do
+      expect(subject.start_distance_m).to eq(10506.0)
+    end
+
+    it "returns the #end_distance in meters" do
+      expect(subject.end_distance_m).to eq(10569.0)
+    end
+
+    it "returns the #total_distance in meters" do
+      expect(subject.total_distance_m).to eq(63.0)
+    end
+
+    it "returns the #type" do
+      expect(subject.type).to eq('speeding')
+    end
+
+    it "returns the #velocity kilometers per hour" do
+      expect(subject.velocity_kph).to eq(112.65)
+    end
+
+    it "returns the #started_at stamp" do
+      expected = DateTime.parse(attributes['started_at'])
+      expect(subject.started_at).to eq(expected)
+    end
+
+    it "returns the #ended_at stamp" do
+      expected = DateTime.parse(attributes['ended_at'])
+      expect(subject.ended_at).to eq(expected)
+    end
+
+    it "returns the #duration in seconds of the event" do
+      expect(subject.duration).to eq(2.0)
+    end
+  end
+end
