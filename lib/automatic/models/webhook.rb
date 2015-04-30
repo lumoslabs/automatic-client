5e90a5b03c0032f76f806db09555763a02edf63b
Nate Klaiber <nate@theklaibers.com>
2015-04-30 09:29:39 -0400

    Adding the core IgnitionOff webhook domain model

--- lib/automatic/models/webhook.rb
+++ lib/automatic/models/webhook.rb
@@ -2,6 +2,9 @@
 require File.expand_path('../webhook/vehicle', __FILE__)
 require File.expand_path('../webhook/user', __FILE__)
 
+# Event models
+require File.expand_path('../webhook/ignition_off', __FILE__)
+
 module Automatic
   module Models
     module Webhook
