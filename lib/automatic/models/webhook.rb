c28d17962b28e55c63ba622767ea80d237c0c943
Nate Klaiber <nate@theklaibers.com>
2015-04-30 10:09:37 -0400

    Adding the core ParkingChanged webhook model
    This will eventually be deprecated. Keeping here
    for current support.

--- lib/automatic/models/webhook.rb
+++ lib/automatic/models/webhook.rb
@@ -8,6 +8,7 @@ require File.expand_path('../webhook/ignition_off', __FILE__)
 require File.expand_path('../webhook/notification_hard_accel', __FILE__)
 require File.expand_path('../webhook/notification_hard_brake', __FILE__)
 require File.expand_path('../webhook/notification_speeding', __FILE__)
+require File.expand_path('../webhook/parking_changed', __FILE__)
 
 module Automatic
   module Models
